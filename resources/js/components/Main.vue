9306b69b9cf09c2e181023893344cd7ec9b8383c
Michael Silverio <michael.silverio@sun-asterisk.com>
2022-02-16 23:09:42 +0800

    Changes to main.js

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,48 +1,45 @@
 <template>
-    <div>
-        <!-- THIS IS THE NAV BAR -->
-        <NavBar></NavBar>
+	<div>
+		<!-- THIS IS THE NAV BAR -->
+		<NavBar></NavBar>
 
-        <!-- THIS IS THE SIDE BAR -->
-        <SideBar></SideBar>
+		<!-- THIS IS THE SIDE BAR -->
+		<SideBar></SideBar>
 
+		<!--   THIS IS THE MAIN CONTENT     -->
+		<router-view></router-view>
 
-        <!--   THIS IS THE MAIN CONTENT     -->
-        <router-view></router-view>
+		<!-- THIS THE FOOTER -->
+		<Footer></Footer>
 
-        <!-- THIS THE FOOTER -->
-        <Footer></Footer>
-
-        <!-- THIS IS THE SIDE OVERLAY -->
-<!--        <SideOverlay></SideOverlay>-->
-
-    </div>
+		<!-- THIS IS THE SIDE OVERLAY -->
+		<!--        <SideOverlay></SideOverlay>-->
+	</div>
 </template>
 
 <script>
-import NavBar from './layouts/NavBar'
-import SideBar from './layouts/Sidebar'
-import Footer from './layouts/Footer'
-import SideOverlay from './layouts/SideOverlay'
+import NavBar from "./layouts/NavBar";
+import SideBar from "./layouts/Sidebar";
+import Footer from "./layouts/Footer";
+import SideOverlay from "./layouts/SideOverlay";
 
 export default {
-    mounted() {
-       console.log('main vue mounted')
-    },
+  mounted() {
+		console.log("main vue mounted");
+	},
 
-    components: {
-        NavBar,
-        SideBar,
-        Footer,
-        SideOverlay
-    }
-}
+	components: {
+		NavBar,
+		SideBar,
+		Footer,
+		SideOverlay,
+	},
+};
 </script>
 
 <style scoped>
 .routes {
-    position: absolute;
-    right: 1px;
+	position: absolute;
+	right: 1px;
 }
 </style>
-
